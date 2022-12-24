e33d843079ecb76932f164054485aa6ba93d6c6c
Antukhan <antu.khan.988@gmail.com>
2022-12-24 23:51:00 +0600

    Renters model created

---
+++ models/renter/index.js
@@ -0,0 +1,38 @@
+// Dependencies
+const mongoose = require('mongoose');
+
+const RenterSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: [true, "Renter Name is required"],
+        select: true,
+    },
+    NID: {
+        type: String,
+        required: [true, "Renter NID Number is required"],
+        select: true,
+    },
+    Phone: {
+        type: String,
+        required: [true, "Renter Phone Number is required"],
+        select: true,
+    },
+    Permanent_Address: {
+        type: String,
+        required: [true, "Renter Permanent Address is required"],
+        select: true,
+    },
+    Building_Name: {
+        type: String,
+        required: [true, "Building Name is required"],
+        select: true,
+    },
+    Apartment_Name: {
+        type: String,
+        required: [true, "Apartment Name is required"],
+        select: true,
+    }
+});
+
+const Renter = mongoose.model("Renter", RenterSchema);
+module.exports = Renter;
